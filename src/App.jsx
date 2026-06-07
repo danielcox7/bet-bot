5a8231d012142e01fea26a4e8deedaa4ba997361
Dan Cox <dan.cox@connect.auto>
2026-06-07 01:12:23 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+      <h1>Project Base Ready</h1>
+      <p>Your web app is ready for development and Netlify deployment.</p>
+    </div>
+  );
+}
+
+export default App;
